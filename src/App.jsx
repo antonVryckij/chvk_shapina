c7e8269764b2363319e355888b3808d010173b13
antonVryckij <antn.vryckij@gmail.com>
2023-02-09 17:12:29 +0200

    initial commit v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <h1>SOme text</h1>
+      <h1>Some text</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
